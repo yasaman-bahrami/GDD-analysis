ed9017b6bc4a38cb9a30444c0227360b009c27b8
haris00 <haristanvir@hotmail.com>
2017-06-13 08:59:20 -0230

    Included main()

--- src/drawGDDPlot.py
+++ src/drawGDDPlot.py
@@ -20,3 +20,27 @@ def drawGDDPlot(gdd, cityName, gColor):
     ax.set_ylabel('GDD based on base_temp=10')
     return plt
     
+def Main():
+    # Taking the arguments from command line. 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-st", dest="stationId", nargs = '*', help="Please provide a list of station Id.")
+    parser.add_argument("-ct", dest="cityName", nargs = '*', help="Please provide a list of city names corresponding to stations.")
+    parser.add_argument("-gc", dest="gColor", nargs = '*', help="Please provide the colors for each city graph.")
+	
+    args = parser.parse_args()
+	
+    cityData = []
+    for i in range(len(args.stationId)):
+    	# Reading the data from downloaded .csv files. 
+        CurrentPath = os.getcwd()
+        FilePath= (CurrentPath+'/CSVData/'+args.cityName[i]+'GDDData.csv')
+        Data, Date, maxTemp, minTemp = getCSVData(FilePath)
+        cityData.append(Data['GDD'])
+    
+    for i in range(len(cityData)):
+        gdd_plt = drawGDDPlot(cityData[i], args.cityName[i], args.gColor[i])
+		
+    gdd_plt.savefig("./plots/GDDPlotIMG.png")
+
+if __name__ == '__main__':
+    Main()
