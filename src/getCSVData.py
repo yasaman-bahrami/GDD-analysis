68b518f5199d0144ab5f06752b4dbc70f4c309c2
Hoda Rafieipour <hodarafiee@Hodas-iMac.local>
2017-06-12 21:35:08 -0230

    put Data, Date, MaxTemp, MinTemp in the arrey

--- src/getCSVData.py
+++ src/getCSVData.py
@@ -3,12 +3,16 @@ import pandas as pd
 import numpy as np
 import pdb
 
-def getCSVData(filePath):
-    gddData = pd.read_csv(filePath, delimiter=',', skiprows=0, encoding='ISO-8859-1')
 
+
+def getCSVData(filePath):
+    try:
+        gddData = pd.read_csv(filePath, delimiter = ',' ,skiprows=0)
+    except:
+        print("Oops! there is problem in reading data", gddData)
 
     Data = pd.DataFrame(gddData)
-    Data.replace('', np.nan, inplace=True)
+    Data.replace('', np.nan, inplace = True)
     Data = Data.dropna()
     rowLen = len(Data.index)
     # if (rowLen > 365):
@@ -17,9 +21,10 @@ def getCSVData(filePath):
     # elif (rowLen < 365):
     #     for i in range(365 - rowLen):
     #         csvData.append(csvData.tail(1), ignore_index=True)
+    Index = Data.keys()
+    Date, maxTemp, minTemp = np.array(Data['Date/Time']),np.array(Data['Max Temp']), np.array(Data['Min Temp'])
 
-
-    # pdb.set_trace()
+    return Data, Date, maxTemp, minTemp
 
 
 
