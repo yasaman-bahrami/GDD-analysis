e7286fba6dd1642f2e228c40d13aae274184d569
Hoda Rafieipour <hrafieipour@mun.ca>
2017-06-14 14:25:17 -0230

    Some chnges to finialize it

--- src/getCSVData.py
+++ src/getCSVData.py
@@ -3,28 +3,19 @@ import pandas as pd
 import numpy as np
 import pdb
 
-""" This function gets a path and reads the result in CSV
+""" This function gets a path and reads the result from CSV in CSV format
 """
 
-def getCSVData(filePath):
-    try:
-        gddData = pd.read_csv(filePath, delimiter = ',' ,skiprows=0)
-    except:
-        print("Oops! there is a problem in reading data", gddData)
-
+def cleanCSV(gddData):
     Data = pd.DataFrame(gddData)
-    Data.replace('', np.nan, inplace = True)
+    Data.replace('', np.nan, inplace=True)
     Data = Data.dropna()
-    rowLen = len(Data.index)
-    # if (rowLen > 365):
-    #     for i in range(rowLen - 365 - 1):
-    #         csvData = csvData.drop(csvData.index[len(csvData) - 1])
-    # elif (rowLen < 365):
-    #     for i in range(365 - rowLen):
-    #         csvData.append(csvData.tail(1), ignore_index=True)
-    Index = Data.keys()
-    Date, maxTemp, minTemp = np.array(Data['Date/Time']),np.array(Data['Max Temp']), np.array(Data['Min Temp'])
+    Date, maxTemp, minTemp = np.array(Data['Date/Time']), np.array(Data['Max Temp']), np.array(Data['Min Temp'])
+    return Data, Date, maxTemp, minTemp
 
+def getCSVData(filePath):
+    gddData = pd.read_csv(filePath, delimiter = ',' ,skiprows=0)
+    Data, Date, maxTemp, minTemp = cleanCSV(gddData)
     return Data, Date, maxTemp, minTemp
 
 
